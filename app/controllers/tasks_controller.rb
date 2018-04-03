e1ba49fb71fc5d8435d3fce89b013e4374c0da45
Lucas Porter-Bakker <lucas.porter.bakker@gmail.com>
2018-04-03 15:51:26 +0800

    Updated: Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,11 +15,13 @@ class TasksController < ApplicationController
   end
 
   def edit
-
+    @task = Task.find(params[:id])
   end
 
   def update
-
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
@@ -30,6 +32,10 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 
 end
